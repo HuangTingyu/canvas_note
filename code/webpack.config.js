27f2bf6f39055fe9af8b00d8903fbe55ec51366a
sakurahuang <100728545@qq.com>
2020-06-30 23:50:46 +0800

    canvas init

---
+++ code/webpack.config.js
@@ -0,0 +1,59 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var { CleanWebpackPlugin } = require('clean-webpack-plugin');
+var webpack = require('webpack');
+
+
+module.exports = {
+  mode:'development',
+  devtool:'eval-cheap-module-source-map',
+  entry: {
+    main:'./src/index.js',
+  },
+  devServer:{
+    contentBase:'./dist',
+    open:true,
+    hot:true,
+    hotOnly:true
+  },
+  module: {
+    rules: [{
+        test: /\.(jpg|png|gif)$/,
+        use: {
+            loader: 'url-loader',
+            // placeholder占位符
+            options: {
+                name: '[name]_[hash].[ext]',
+                outputPath: 'images/',
+                limit: 20480
+            }
+        }
+    },{
+        test:/\.css$/,
+        use:[
+          'style-loader',
+          'css-loader',
+          'postcss-loader'
+        ]
+    },{
+      test:/\.art$/,
+      use: {
+        loader:'art-template-loader'
+      }
+    }
+  ]
+},
+  output: {
+    publicPath:'/',
+    filename:'[name].js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template:'./src/index.html'}),
+    new webpack.ProvidePlugin({
+      $:'jquery'
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
