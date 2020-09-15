ab533ea0bf625325c29f0e92a4119186e9499676
sakurahuang <100728545@qq.com>
2020-09-15 20:36:47 +0800

    增加点击事件

--- code/src/js/processImg/basis.js
+++ code/src/js/processImg/basis.js
@@ -2,6 +2,7 @@ var cnv = $('#canvas')[0]
 var cxt = cnv.getContext('2d')
 
 import basisImgPath from 'assets/canvas.jpg'
+import testPath from 'assets/basisImg.png'
 var image = new Image()
 image.src = basisImgPath
 cnv.height = 600 * (4270 / 6406)
@@ -18,7 +19,17 @@ $('.canvas_wrap').mouseover(function(){
     $('#canvas').css('opacity', '0.5')  
 })
 
-$('.canvas_wrap').mousemove(function(e){
+let bg_size = String(cnv.width)+'px'+' '+String(cnv.height)+'px'
+$('.canvas_wrap').css({
+    'background-image': 'url('+ basisImgPath +')',
+    'background-position': '0 0',
+    'background-size':  bg_size,
+    'background-repeat': 'no-repeat'
+})
+
+
+
+var wrap_move = function(e){
     let canvas_width = $('#canvas').width()
     let canvas_height=$('#canvas').height()
 
@@ -32,8 +43,6 @@ $('.canvas_wrap').mousemove(function(e){
     let mouse_boundary_x = canvas_width - (wrap_width/2)
     let wrap_left_boundary = canvas_width - wrap_width
 
-    let boundary_y = $('#canvas').height() - $('.canvas_wrap').height()
-    
     let y_mouse = e.pageY
     let wrap_top = y_mouse - (wrap_height/2)
     let mouse_start_boundary_y = wrap_height/2
@@ -58,6 +67,23 @@ $('.canvas_wrap').mousemove(function(e){
     } 
     else if (y_mouse <= mouse_start_boundary_y){
         $('.canvas_wrap').css('top', 0 + 'px') 
+    }
+    let top = $('.canvas_wrap').offset().top
+    let left = $('.canvas_wrap').offset().left
+
+    var bg_pos = '-'+left + 'px -' + top + 'px'
+    $('.canvas_wrap').css('background-position', bg_pos)
+}
+
+$('.canvas_wrap').mousemove(wrap_move)
+
+$('.canvas_wrap').click(function(){
+    // 
+    var objEvent = $._data($('.canvas_wrap')[0], 'events')
+    if(objEvent['mousemove']){
+        $('.canvas_wrap').unbind('mousemove')
+    } else {
+        $('.canvas_wrap').mousemove(wrap_move) 
     }
 })
 
